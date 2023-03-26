e4ac79951d5f3e781b2e6d4e928a53b8b42b367a
Родин Юрий Александрович <derek.ass98@Mail.ru>
2023-03-26 22:00:45 +0300

    Task 2

---
+++ Task2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите первое число: ");
+int namber1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int namber2 = int.Parse(Console.ReadLine());
+if (namber1 < namber2) {
+    Console.WriteLine("max = " + namber2);
+    Console.WriteLine("mix = " + namber1);
+} else if (namber1 > namber2) {
+    Console.WriteLine("max = " + namber1);
+    Console.WriteLine("mix = " + namber2);
+} else {
+    Console.WriteLine("Числа равны!");
+}
