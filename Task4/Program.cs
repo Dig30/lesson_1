542914854cb7f1fc45d973148afb2aa76bb4a95e
Родин Юрий Александрович <derek.ass98@Mail.ru>
2023-03-26 22:49:27 +0300

    Task 4

---
+++ Task4/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Введи первое число: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введи второе число: ");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine("Введи третье число: ");
+int c = int.Parse(Console.ReadLine());
+
+if (a >= b) {
+    if (a >= c) { 
+        Console.WriteLine("max = " + a);
+    } else {
+        Console.WriteLine("max = " + c);
+    }   
+} else {
+    if (b >= c) {
+        Console.WriteLine("max = " + b);
+        
+    } else {
+        Console.WriteLine("max = " + c);
+    }
+}
+
+
+
